a67e89b2a597b6230cede1fbc7abd2aae863b28d
raghavk31 <raghav@perspectives.community>
2026-05-02 16:18:38 +0530

    Update content.js

--- content.js
+++ content.js
@@ -47,7 +47,7 @@ const CONTENT = {
       // Add image paths here as you collect them
       // Example: "images/iclei/malaysia-plan.jpg"
       images: [
-        "images/ICLEI/Screenshot 2026-04-22 134308.png",
+        "images/ICLEI/placeholder.png",
         // "images/iclei/malaysia.jpg",
         // "images/iclei/workshop.jpg",
       ],
